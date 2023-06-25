7387533741d9ba24792a45de548483dd1896b98f
TinyDoowy <58040941+TinyDoowy@users.noreply.github.com>
2023-06-25 11:15:52 +0200

    test bon salon

--- index.js
+++ index.js
@@ -218,7 +218,7 @@ bot.on('message', async function (message, user) {
 
     //commande Staff pour tournoi (salon staff monche)
     //if(message.member.roles.cache.has(auth.server.role.staff)&&message.channel.id==auth.server.salon.staffmonche){
-    if(message.member.roles.cache.has(auth.server.role.staff)){
+    if(message.channel.id==auth.server.salon.staffmonche){
 	    console.log("bon role bon channel");
         if(petitMessage.startsWith(prefixTournoiOn)){
             message.delete();
